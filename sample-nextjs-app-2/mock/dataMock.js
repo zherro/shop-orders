e1881a1efa9d96b4ae89840548ad92a6739f5bd7
Cleiton Oliveira <cleitonoliveira_SouthSystem@getnet.com.br>
2022-02-22 21:05:40 -0300

    limpeza de processo

---
+++ sample-nextjs-app-2/mock/dataMock.js
@@ -0,0 +1,32 @@
+ export default [
+	{
+		id: 1010,
+		name: "Product 1",
+		qtd: 12,
+		price: 10.10,
+	},
+	{
+		id: 1013,
+		name: "Product 2",
+		qtd: 12,
+		price: 10.10,
+	},
+	{
+		id: 1011,
+		name: "Product 4",
+		qtd: 12,
+		price: 10.10,
+	},
+	{
+		id: 1015,
+		name: "Product 5",
+		qtd: 12,
+		price: 10.10,
+	},
+	{
+		id: 1015,
+		name: "Product 6",
+		qtd: 12,
+		price: 10.10,
+	},
+]
--- sample-nextjs-app-2/pages/products/index.js
+++ sample-nextjs-app-2/pages/products/index.js
@@ -1,90 +1,25 @@
 import { useEffect, useState } from 'react';
 import styles from '../../styles/Products.module.css'
 
-const jSampleData = [
-	{
-		id: 1010,
-		name: "Product 1",
-		qtd: 12,
-		price: 10.10,
-	},
-	{
-		id: 1013,
-		name: "Product 2",
-		qtd: 12,
-		price: 10.10,
-	},
-	{
-		id: 1011,
-		name: "Product 4",
-		qtd: 12,
-		price: 10.10,
-	},
-	{
-		id: 1015,
-		name: "Product 5",
-		qtd: 12,
-		price: 10.10,
-	},
-	{
-		id: 1015,
-		name: "Product 6",
-		qtd: 12,
-		price: 10.10,
-	},
-];
+import jSampleData from '../../mock/dataMock'
+
 
 const Products = () => {
-
-	function funAddToCart(item) {
-		aCart.push(item)
-
-		setIContCart(iContCart + 1)
-	}
-
-/*
-	const funAddToCart = (item) => {
-		aCart.push(item)
-
-		setIContCart(iContCart + 1)
-	}
-*/
-
-	const [iContEffect, setIContEffect] = useState(0);
 
 	const [aCart, setACart] = useState([]);
 	const [iContCart, setIContCart] = useState(0);
 	const [eCartAmmout, setECartAmmount] = useState(0);
 
-	
-	const [iContaUpdatesMap, setIContaUpdatesMap] = useState(0);
-
-	
-	const [testeRefresComponent, addMaisUm] = useState(0);
-
-	// let testeRefresComponent = 0;
-
-	//  useEffect(() => {
-	//  	setIContEffect(iContEffect + 1);
-	//  })
-
-	const countItemRefres = () => {
-		addMaisUm(testeRefresComponent + 1);
-	}
-
-	useEffect(() => {
-
-		setIContaUpdatesMap(iContaUpdatesMap + 1);
-
-	}, [jSampleData] );
 
 	return <div>
-		{ /* <h2 className={styles.title} >Products - { iContEffect } </h2> */ }
+	
+		<h2 className={styles.title} >Products </h2>
 
-		<h2 className={styles.title} >Products - { iContaUpdatesMap } </h2>
 
+		<br />
 		<div>QTD: { iContCart } </div>
 		<div>Total: { eCartAmmout } </div>
+		<br />
 
 		{
 			jSampleData.map((item, index) => {
@@ -94,14 +29,11 @@ const Products = () => {
 				return <div className={styles.boxProducts} key={index} >
 						<div className={styles.cardProduct} >
 							<div className={styles.productName} > { item.name } </div>
-						{ /*	<span className={styles.productCode}> { item.id } </span>
+							<span className={styles.productCode}> { item.id } </span>
 							<div className={styles.productqtd} >qtd: { item.qtd }</div>
 							<div className={styles.productValue}> { item.price } </div>
 
-							<button
-								onClick={() => funAddToCart(item)}
-								className={styles.btn}>+ add</button>
-						*/ }
+							<button className={styles.btn}>+ add</button>
 						</div>
 					</div>
 
